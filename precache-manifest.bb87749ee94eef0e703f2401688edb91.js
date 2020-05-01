31915c60e71dba51a56e74063d57b197b642109a
giu93iaco <giu93iaco@gmail.com>
2020-05-01 22:04:32 +0200

    Updates

---
+++ precache-manifest.bb87749ee94eef0e703f2401688edb91.js
@@ -0,0 +1,322 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220e3ff2575d7cabbdbc82aae34256c5",
+    "url": "/giocobimbi/index.html"
+  },
+  {
+    "revision": "c49b016843998a5e6720",
+    "url": "/giocobimbi/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "61708dcac3e3b89fc57b",
+    "url": "/giocobimbi/static/css/main.0b44cb6a.chunk.css"
+  },
+  {
+    "revision": "c49b016843998a5e6720",
+    "url": "/giocobimbi/static/js/2.90c079df.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/giocobimbi/static/js/2.90c079df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61708dcac3e3b89fc57b",
+    "url": "/giocobimbi/static/js/main.ac841ef9.chunk.js"
+  },
+  {
+    "revision": "e17f16d7c82274c7f547",
+    "url": "/giocobimbi/static/js/runtime-main.66ca8fd2.js"
+  },
+  {
+    "revision": "abfa2ef92de743c97558eb810bfb086f",
+    "url": "/giocobimbi/static/media/abito.abfa2ef9.png"
+  },
+  {
+    "revision": "eab312bd1c13b0a5874716492e597e5d",
+    "url": "/giocobimbi/static/media/aereo.eab312bd.png"
+  },
+  {
+    "revision": "57d745854f5c93f4c2ad65bb50debb9d",
+    "url": "/giocobimbi/static/media/ananas.57d74585.png"
+  },
+  {
+    "revision": "398b1796539a3d97b579b838f45022b9",
+    "url": "/giocobimbi/static/media/arancia.398b1796.png"
+  },
+  {
+    "revision": "2d9bf2c9e1eb7d63ae0984d786b1d9cf",
+    "url": "/giocobimbi/static/media/autobus.2d9bf2c9.png"
+  },
+  {
+    "revision": "3e8cb24a748f6b3db67c47923a4305c3",
+    "url": "/giocobimbi/static/media/automobile.3e8cb24a.png"
+  },
+  {
+    "revision": "50546a36424cfaa20ad774f94a2e6459",
+    "url": "/giocobimbi/static/media/avocado.50546a36.png"
+  },
+  {
+    "revision": "58cc49faa642cde9d57d2552d475e6d9",
+    "url": "/giocobimbi/static/media/balena.58cc49fa.png"
+  },
+  {
+    "revision": "2d24f8f5eceabfd7f3fbc06f74198c31",
+    "url": "/giocobimbi/static/media/banana.2d24f8f5.png"
+  },
+  {
+    "revision": "effa7cff64b1b8383402e85ffa9d8323",
+    "url": "/giocobimbi/static/media/bicicletta.effa7cff.png"
+  },
+  {
+    "revision": "bac56fc034951523845d1d9ccfcedd4c",
+    "url": "/giocobimbi/static/media/birra.bac56fc0.png"
+  },
+  {
+    "revision": "47aafc10e7b0ae41fe293e6f95e040ef",
+    "url": "/giocobimbi/static/media/body.47aafc10.png"
+  },
+  {
+    "revision": "6899973cff5d603eb59c54ac639f5348",
+    "url": "/giocobimbi/static/media/borsa.6899973c.png"
+  },
+  {
+    "revision": "be63bfea33991c3eb68019caf061aa4b",
+    "url": "/giocobimbi/static/media/bracciale.be63bfea.png"
+  },
+  {
+    "revision": "9f6ba4092acf7a7bd4e78806acb55ec1",
+    "url": "/giocobimbi/static/media/caffè.9f6ba409.png"
+  },
+  {
+    "revision": "8f139f7270802c20f4d514c6a3b0a434",
+    "url": "/giocobimbi/static/media/calzino.8f139f72.png"
+  },
+  {
+    "revision": "3cea2e1ada395a5f96a0bf0088de3e95",
+    "url": "/giocobimbi/static/media/cane.3cea2e1a.png"
+  },
+  {
+    "revision": "7a8ac23ea85a3fa01b90e800e39c1bf7",
+    "url": "/giocobimbi/static/media/cappello.7a8ac23e.png"
+  },
+  {
+    "revision": "7cbda50c77c00586025cf36f870973dc",
+    "url": "/giocobimbi/static/media/cavallo.7cbda50c.png"
+  },
+  {
+    "revision": "bb923b1fb5a1c23c86261f58760c2034",
+    "url": "/giocobimbi/static/media/cavalluccio.bb923b1f.png"
+  },
+  {
+    "revision": "f6c41df4432e2368d5c49920c3ee8da2",
+    "url": "/giocobimbi/static/media/ciabatta.f6c41df4.png"
+  },
+  {
+    "revision": "692f88ab3360efd2bc2c136e6ebb4235",
+    "url": "/giocobimbi/static/media/ciambella.692f88ab.png"
+  },
+  {
+    "revision": "40c2def77a3f41a14a6f501492fd6c2d",
+    "url": "/giocobimbi/static/media/cioccolata_calda.40c2def7.png"
+  },
+  {
+    "revision": "0fc15838931a24ec5b167840ad0c78ae",
+    "url": "/giocobimbi/static/media/coccodrillo.0fc15838.png"
+  },
+  {
+    "revision": "e4ef51d97aff2461ff1cab8b8ffdf80a",
+    "url": "/giocobimbi/static/media/cocomero.e4ef51d9.png"
+  },
+  {
+    "revision": "7d2119ce13fa817e874347da6b2c9f24",
+    "url": "/giocobimbi/static/media/corallo.7d2119ce.png"
+  },
+  {
+    "revision": "cf2e5f4e07f008ff3f83dee4054e3834",
+    "url": "/giocobimbi/static/media/corona.cf2e5f4e.png"
+  },
+  {
+    "revision": "c66c23f331209f58508c0ee2d16d9540",
+    "url": "/giocobimbi/static/media/costume.c66c23f3.png"
+  },
+  {
+    "revision": "f6e2521bad27ccc13f35c1ba50d9911a",
+    "url": "/giocobimbi/static/media/elefante.f6e2521b.png"
+  },
+  {
+    "revision": "a733d3471cc6ec0ce82ce85dbb8add11",
+    "url": "/giocobimbi/static/media/foca.a733d347.png"
+  },
+  {
+    "revision": "3a37b03787930cd8112b276294a42813",
+    "url": "/giocobimbi/static/media/fragola.3a37b037.png"
+  },
+  {
+    "revision": "7f070a423c504551c3685580ac4b9869",
+    "url": "/giocobimbi/static/media/furgone.7f070a42.png"
+  },
+  {
+    "revision": "e3ce9b9265e7b3a9edba45b86a2bd7f4",
+    "url": "/giocobimbi/static/media/giraffa.e3ce9b92.png"
+  },
+  {
+    "revision": "1ea3a49ef278409cddc8c937249dea11",
+    "url": "/giocobimbi/static/media/granchio.1ea3a49e.png"
+  },
+  {
+    "revision": "e7e1ed56714c812266d0968e1022989a",
+    "url": "/giocobimbi/static/media/grembiule.e7e1ed56.png"
+  },
+  {
+    "revision": "ef234c9fbdafad148c1e5dc880fa5de9",
+    "url": "/giocobimbi/static/media/leone.ef234c9f.png"
+  },
+  {
+    "revision": "ec26860334e46ab514752359caf752a3",
+    "url": "/giocobimbi/static/media/leopardo.ec268603.png"
+  },
+  {
+    "revision": "79b425be1368a2ee6471ee1cb2d44294",
+    "url": "/giocobimbi/static/media/lepre.79b425be.png"
+  },
+  {
+    "revision": "b17d955b20983db653a76df8b848af13",
+    "url": "/giocobimbi/static/media/limone.b17d955b.png"
+  },
+  {
+    "revision": "cfe2c91c4ce77ffb447fb0bdab63ff0d",
+    "url": "/giocobimbi/static/media/maglietta.cfe2c91c.png"
+  },
+  {
+    "revision": "686aae61d9a3ec7328d90c5dea4f58ec",
+    "url": "/giocobimbi/static/media/maiale.686aae61.png"
+  },
+  {
+    "revision": "72ba613c1e19af8901d35bd821adf0a6",
+    "url": "/giocobimbi/static/media/mirtilli.72ba613c.png"
+  },
+  {
+    "revision": "523c0c2605c361cfbaa007472464ed49",
+    "url": "/giocobimbi/static/media/moto.523c0c26.png"
+  },
+  {
+    "revision": "8c2280db58c2846442ca6a01c5c7405e",
+    "url": "/giocobimbi/static/media/mucca.8c2280db.png"
+  },
+  {
+    "revision": "f8c6fba45d93a63a771c9c0bf15963ac",
+    "url": "/giocobimbi/static/media/muffin.f8c6fba4.png"
+  },
+  {
+    "revision": "0a2d841d74ad1e0bc0f7e9315e9935a3",
+    "url": "/giocobimbi/static/media/nave.0a2d841d.png"
+  },
+  {
+    "revision": "beb05c4c87961aa79c3265c781a8dd74",
+    "url": "/giocobimbi/static/media/ombrello.beb05c4c.png"
+  },
+  {
+    "revision": "90bc11e15efadc44fb470135074adad6",
+    "url": "/giocobimbi/static/media/orso.90bc11e1.png"
+  },
+  {
+    "revision": "cbdb2da1e9e27af9982ee70ea56b01db",
+    "url": "/giocobimbi/static/media/panino.cbdb2da1.png"
+  },
+  {
+    "revision": "a1fc8f18e2786bd7abccaf9032a6a895",
+    "url": "/giocobimbi/static/media/pantaloni.a1fc8f18.png"
+  },
+  {
+    "revision": "1bf59eac8d648bf8ba9a76465c514916",
+    "url": "/giocobimbi/static/media/pappagallo.1bf59eac.png"
+  },
+  {
+    "revision": "c471b8a54ea120aef3550f2ea6554242",
+    "url": "/giocobimbi/static/media/pecora.c471b8a5.png"
+  },
+  {
+    "revision": "27f6df763f5fa5283923bc3d8ed85b93",
+    "url": "/giocobimbi/static/media/pera.27f6df76.png"
+  },
+  {
+    "revision": "447a8581ec3f22839ec54b2dda263838",
+    "url": "/giocobimbi/static/media/pesca.447a8581.png"
+  },
+  {
+    "revision": "f4599f238f101ac3a557bb85b806e65a",
+    "url": "/giocobimbi/static/media/pesce.f4599f23.png"
+  },
+  {
+    "revision": "bd0f71d8e4ca247c55bfcaade95f0a24",
+    "url": "/giocobimbi/static/media/pinguino.bd0f71d8.png"
+  },
+  {
+    "revision": "a6bd30e80cc30f3276737f3c8a5bcfc7",
+    "url": "/giocobimbi/static/media/pipistrello.a6bd30e8.png"
+  },
+  {
+    "revision": "594d8af1b7c813657d4d4638da6947b2",
+    "url": "/giocobimbi/static/media/polpo.594d8af1.png"
+  },
+  {
+    "revision": "472064725754f521f47c114aad3b7dbb",
+    "url": "/giocobimbi/static/media/pulcino.47206472.png"
+  },
+  {
+    "revision": "26d7643387deaff2f8a5afe25b048161",
+    "url": "/giocobimbi/static/media/rana.26d76433.png"
+  },
+  {
+    "revision": "df86d86d2aecc3b10357f952f81a98e0",
+    "url": "/giocobimbi/static/media/rinoceronte.df86d86d.png"
+  },
+  {
+    "revision": "37378a44f2a8815c8d3628ec217fdb0d",
+    "url": "/giocobimbi/static/media/scarpa.37378a44.png"
+  },
+  {
+    "revision": "3a191f50e54c62b58d4b12adf754afe4",
+    "url": "/giocobimbi/static/media/scimmia.3a191f50.png"
+  },
+  {
+    "revision": "82689212ffa8a33e69805fb373d42012",
+    "url": "/giocobimbi/static/media/serpente.82689212.png"
+  },
+  {
+    "revision": "d8a587add2d08251547109b21f6d8894",
+    "url": "/giocobimbi/static/media/squalo.d8a587ad.png"
+  },
+  {
+    "revision": "20e5da453c69b6ad315e4806935a2f84",
+    "url": "/giocobimbi/static/media/taxi.20e5da45.png"
+  },
+  {
+    "revision": "b489ddca5e1fef7a8b498ceb466fe274",
+    "url": "/giocobimbi/static/media/tigre.b489ddca.png"
+  },
+  {
+    "revision": "a7a7e3c608115b98adb499ff5480a61b",
+    "url": "/giocobimbi/static/media/toro.a7a7e3c6.png"
+  },
+  {
+    "revision": "783c2914108ed3beac57073347d318b0",
+    "url": "/giocobimbi/static/media/treno.783c2914.png"
+  },
+  {
+    "revision": "fb9b2bc07b117bf7c98c2db4634a1006",
+    "url": "/giocobimbi/static/media/uva.fb9b2bc0.png"
+  },
+  {
+    "revision": "2f77aec1fbcf75697c93c834b2103d86",
+    "url": "/giocobimbi/static/media/vino.2f77aec1.png"
+  },
+  {
+    "revision": "d1ac6aa90650d118fc2916fc4420c9ab",
+    "url": "/giocobimbi/static/media/volpe.d1ac6aa9.png"
+  },
+  {
+    "revision": "8973a2c1a8a1c5d0be4158db39d327fd",
+    "url": "/giocobimbi/static/media/zebra.8973a2c1.png"
+  }
+]);
